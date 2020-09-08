ba25c3803b842f9284ef10288ee888ed9c619039
mkolobov <mkolobov@playflock.com>
2020-09-09 00:45:49 +0300

    without video, wait for models

--- AR_app/Assets/NextGenRecorder/MonoBehaviours/VirtualScreen.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace pmjo.NextGenRecorder
-{
-    [AddComponentMenu("Next Gen Recorder/Virtual Screen")]
-    public class VirtualScreen : Recorder.VirtualScreenBase
-    {
-    }
-}
