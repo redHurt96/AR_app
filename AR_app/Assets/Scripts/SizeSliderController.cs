b8c056ae1b43b55d89ec06e01d488375c1f51779
avdrine <avdrine@gmail.com>
2020-09-23 00:12:28 +0300

    Скейл моделек

---
+++ AR_app/Assets/Scripts/SizeSliderController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeSliderController : MonoBehaviour
+{
+    public Transform[] resizableObjects;
+    
+
+    public void ChangeSize(float newSize)
+    {
+        for (int i = 0; i < resizableObjects.Length; i++)
+        {
+            resizableObjects[i].localScale = new Vector3(newSize, newSize, newSize);
+        }
+    }    
+}
