f991cc64077947810dc8b1995dbcf82fa4c41818
avdrine <avdrine@gmail.com>
2020-09-23 12:13:53 +0300

    Правка плашки для интерфейса + скрипт для съемки скринов для сторов

---
+++ AR_app/Assets/Scripts/TestScreenshot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TestScreenshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            StartCoroutine(TakeScrShot());
+        }
+    }
+    private IEnumerator TakeScrShot()
+    {
+        yield return new WaitForEndOfFrame();
+
+
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+
+        var bytes = tex.EncodeToPNG();
+        Destroy(tex);
+
+        File.WriteAllBytes(Application.persistentDataPath + "/screenshot.png", bytes);
+        Debug.Log(Application.persistentDataPath + "/screenshot.png");
+
+    }
+}
