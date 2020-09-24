895006527a0cb1a41daf786d8f71586cc03c215d
avdrine <avdrine@gmail.com>
2020-09-24 14:56:32 +0300

    Изменение диапазона скейлов, изменения позиций и иконки

--- AR_app/Assets/Scripts/SizeSliderController.cs
+++ AR_app/Assets/Scripts/SizeSliderController.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SizeSliderController : MonoBehaviour
 {
     public Transform[] resizableObjects;
-    
 
+
+    private void Start()
+    {
+        float startSize = GetComponent<Slider>().value;
+        for (int i = 0; i < resizableObjects.Length; i++)
+        {
+            resizableObjects[i].localScale = new Vector3(startSize, startSize, startSize);
+        }
+    }
     public void ChangeSize(float newSize)
     {
         for (int i = 0; i < resizableObjects.Length; i++)
